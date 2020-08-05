29e08581c6ba97959593f2d19d53027384469d82
aksuz <zuzanna.burzynska@interia.pl>
2020-08-05 19:50:29 +0200

    innitial commit

---
+++ src/main/java/edu/tul/beautyscanner/BeautyScannerApplication.java
@@ -0,0 +1,13 @@
+package edu.tul.beautyscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tul/beautyscanner/BeautyScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tul.beautyscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
